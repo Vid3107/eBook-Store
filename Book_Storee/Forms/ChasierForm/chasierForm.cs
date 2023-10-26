b331c39f927274de0e6154ff542d799e24a48f20
SokunPanha <sokunpanha.panha@gmail.com>
2023-10-26 22:54:51 +0700

    working on cashier from

---
+++ Book_Storee/Forms/ChasierForm/chasierForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Book_Storee.Forms.ChasierForm
+{
+    public partial class chasierForm : Form
+    {
+        public chasierForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
